54504eeb07fcc76fb06dfa9f4002f212972f4a31
Shiraz Esat <shiraz.esat@gmail.com>
2022-09-26 13:07:58 +0100

    Update intro text

--- pages/index.js
+++ pages/index.js
@@ -21,13 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>
-          Howdy - been using React with CRA for some time. Now trying Next.js
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Simple React app written with next.js</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
